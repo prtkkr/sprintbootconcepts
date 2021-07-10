b01dca79845deb55da8cda12bcb84bbc73c1c463
Pratik Kumar <pratikkumarsbg@gmail.com>
2021-07-10 23:26:25 +0530

    spring boot concepts

---
+++ src/main/java/com/demo/springbootconcepts/SpringbootconceptsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootconceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootconceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/springbootconcepts/SpringbootconceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootconceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
